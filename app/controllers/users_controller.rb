e0c271c1cf118cdf3578273a0532febba6761ddb
tech-kazuki <uekazuki0411@akiyamakazukinoMacBook-puro.local>
2019-03-22 20:01:37 +0900

    Add folders files

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,19 @@
+class UsersController < ApplicationController
+
+  def edit
+  end
+
+  def update
+    if current_user.update(user_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def params_permit
+    params.require(:user).permit(:name, :email)
+  end
+end
