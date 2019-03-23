891fc4e1dd8cb5087ed4a7e03860cc0843afc663
tech-kazuki <uekazuki0411@akiyamakazukinoMacBook-puro.local>
2019-03-23 16:50:37 +0900

    Add group

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'messages#index'
-  resources :users, only: [:edit, :update, :destroy, :create]
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :groups, only: [:new, :create, :edit, :update]
+  resources :users, only: [:edit, :update]
 end
