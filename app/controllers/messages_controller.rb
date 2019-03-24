2a1935f436c318f2d550ba02a91b43664b70e87d
tech-kazuki <uekazuki0411@akiyamakazukinoMacBook-puro.local>
2019-03-24 20:29:16 +0900

    Recover message_controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -7,9 +7,9 @@ class MessagesController < ApplicationController
   end
 
   def create
-    @message = @group.message.new(message_params)
+    @message = @group.messages.new(message_params)
     if @message.save
-      redirect_to group_messaegs_path(@group), notice: 'メッセージが送信されました'
+      redirect_to group_messages_path(@group), notice: 'メッセージが送信されました'
     else
       @messages = @group.messages.includes(:user)
       flash.now[:alert] = 'メッセージを入力してください。'
@@ -20,7 +20,7 @@ class MessagesController < ApplicationController
   private
 
   def message_params
-    poarams.require(:message).permit(:content, :image).merge(user_id: current_user.id)
+    params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
   end
 
   def set_group
