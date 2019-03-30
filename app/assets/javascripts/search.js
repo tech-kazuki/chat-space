3e5c056057cc3fcda392a1419dd327832dd6295d
tech-kazuki <uekazuki0411@akiyamakazukinoMacBook-puro.local>
2019-03-30 18:20:06 +0900

    Add searchAPI

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -3,24 +3,33 @@ $(document).on('turbolinks:load', function() {
 var search_list = $('#user-search-result');
 
 function appendUser(data) {
-  var html = `<div class="chat-group-user clearfix">
+  var html = `<div id="chat-group-user-new" class="chat-group-user clearfix">
                 <p class="chat-group-user__name">${ data.name }</p>
                 <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${ data.id }" data-user-name="${ data.name }">追加</a>
               </div>`
     search_list.append(html);
   }
 
-  function appendErrMsgToHTML(msg) {
-    var html = `<div class="chat-group-user clearfix">
-                  <p class="chat-group-user__name">${ msg }</p>
-                </div>`
+function appendErrMsgToHTML(msg) {
+  var html = `<div class="chat-group-user clearfix">
+                <p class="chat-group-user__name">${ msg }</p>
+              </div>`
     search_list.append(html);
   }
 
+var add_member = $('#chat-group-users');
+
+function addGroupUserHTML(data) {
+  var html = `<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-8'>
+                            <input name='group[user_ids][]' type='hidden' value='${ data.userId }'>
+                              <p class='chat-group-user__name'>${ data.userName }</p>
+                              <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
+                        </div>`;
+    add_member.append(html)
+}
 
   $('#user-search-field').on('keyup', function() {
     var input = $(this).val();
-    var
     $.ajax({
       type: 'GET',
       url: '/users',
@@ -41,5 +50,17 @@ function appendUser(data) {
     .fail(function() {
       alert('ユーザーの検索に失敗しました');
     })
-  })
+  });
+
+  $(document).on('click', '.user-search-add', function() {
+    userData = $(this).data();
+    debugger;
+    console.log(userData);
+    addGroupUserHTML(userData);
+    $('#chat-group-user-new').remove();
+  });
+
+  $(document).on('click', '.js-chat-member', function() {
+    $(this).remove();
+  });
 });
