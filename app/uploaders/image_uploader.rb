12ffbd29be3013a066ea7acad2b6119753002e87
tech-kazuki <uekazuki0411@akiyamakazukinoMacBook-puro.local>
2019-04-06 15:35:22 +0900

    Edit file

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -7,4 +7,8 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   process resize_to_fit: [800, 800]
 
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+
 end
