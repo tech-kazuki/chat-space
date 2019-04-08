58117c3e6de555d264160ca05c5d1eba9302cccc
tech-kazuki <uekazuki0411@akiyamakazukinoMacBook-puro.local>
2019-04-08 16:26:01 +0900

    Delete html

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,7 +4,6 @@ class MessagesController < ApplicationController
     @message = Message.new
     @messages = @group.messages.includes(:user)
     respond_to do |format|
-        format.html
         format.json { @new_messages = @messages.where('id > ?', params[:message][:id])}
     end
   end
